136083e39e88f91eb67857b3200e69bdecbba481
tainamartin <142798152+tainamartin@users.noreply.github.com>
2024-10-29 15:08:27 +0000

    .

--- perguntas.js
+++ perguntas.js
@@ -15,3 +15,40 @@ criaCartao(
     'O que é uma função?',
     'Uma função é um bloco de código que executa alguma tarefa'
 )
+
+criaCartao(
+    'Lingua francesa',
+    'Como se diz OI em francês?',
+    'Oi em ingles é SALUT (SALU)'
+) 
+
+criaCartao(
+    'Lingua francesa',
+    'Como se diz BOM DIA em francês?',
+    'Oi em ingles é BONJOUR (BONJÚr)'
+) 
+
+
+criaCartao(
+    'lingua ingles',
+    'Como se diz praia em inglês?',
+    'Oi em ingles é BEACH (SALU)'
+) 
+
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz OI em francês?',
+    'Oi em ingles é SALUT (SALU)'
+) 
+
+
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz OI em francês?',
+    'Oi em ingles é SALUT (SALU)'
+) 
+
+
+
